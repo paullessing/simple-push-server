aa13f40fbe4a3f9541e208eac822c78cbd9f5111
paul.lessing <paul@paullessing.com>
2018-09-01 13:57:57 +0100

    Initial commit with static files

---
+++ handler.ts
@@ -0,0 +1,28 @@
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+// @ts-ignore
+import * as StaticFileHandler from 'serverless-aws-static-file-handler';
+
+// export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+//       input: event,
+//     }),
+//   };
+//
+//   cb(null, response);
+// };
+
+export const staticFiles = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  const clientFilesPath = './frontend/';
+  if (event.path === '/' || !event.path) {
+    event = { ...event, path: '/index.html' };
+  }
+  try {
+    const response = await new StaticFileHandler(clientFilesPath).get(event, context);
+    callback(null, response);
+  } catch (e) {
+    callback(null, { statusCode: 500, body: JSON.stringify(e.stack) });
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const plugins = [
+  new CopyWebpackPlugin([
+    {
+      from: 'frontend/**',
+    },
+  ])
+];
+
+const entries = {};
+
+Object.keys(slsw.lib.entries).forEach(
+  key => (entries[key] = ['./source-map-install.js', slsw.lib.entries[key]])
+);
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  plugins,
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+};
